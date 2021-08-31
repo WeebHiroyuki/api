00f2212fafef24d4f20c34f846aabe2e8697fa31
Hiroyuki <hello@computersurgeon.dev>
2021-08-31 19:01:31 -0400

    feat(routes/emoji): find image URL

---
+++ routers/emoji/constants.py
@@ -0,0 +1,1 @@
+EMOJI_PROVIDERS = ["apple", "google", "microsoft", "samsung", "whatsapp", "twitter", "facebook", "joypixels", "openmoji", "emojidex", "messenger", "lg", "htc", "mozilla", "softbank", "docomo"]
--- routers/emoji/emoji.py
+++ routers/emoji/emoji.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from emojipedia import Emojipedia
+from constants import EMOJI_PROVIDERS
 
 router = APIRouter(
     prefix="/emoji",
@@ -11,3 +13,34 @@ def root():
     return {
         "detail": "This is the base URL for the emoji endpoints. Check the documentation for more information."
     }
+
+@router.get("/:{emoji_name}")
+def resolve_emoji_url(emoji_name: str, provider: str):
+    if emoji_name.startswith(":") or emoji_name.endswith(":"):
+        return {
+            "detail": "Please omit any colons from the ends of your emoji name."
+        }
+
+    if provider not in EMOJI_PROVIDERS:
+        return {
+            "detail": "Please specify a valid platform."
+        }
+
+    emoji = Emojipedia.search(emoji_name)
+
+    if emoji is None:
+        return {
+            "detail": f"The requested emoji ('{emoji_name}') could not be found."
+        }
+
+    url: str = None
+    for platform in emoji.platforms:
+        if provider.lower() == platform.name.lower():
+            url = platform.image_url
+
+    if url is None:
+        return {
+            "detail": f"I couldn't find a platform where the {emoji_name} emoji is available."
+        }
+
+    return { url }
